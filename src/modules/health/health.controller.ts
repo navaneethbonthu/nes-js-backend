fb7faac9521e4a3c340a042d2112493c6c4a91f2
navaneethbonthu <navaneethbonthu@gmail.com>
2026-09-01 16:53:26 +0530

    disk healt removed

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -20,7 +20,7 @@ export class HealthController {
         return this.healt.check([
             () => this.db.isHealty('database'),
             () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
-            () => this.disk.checkStorage('storage', { thresholdPercent: 0.9, path: process.platform === 'win32' ? 'c:\\' : '/' })
+            // () => this.disk.checkStorage('storage', { thresholdPercent: 0.9, path: process.platform === 'win32' ? 'c:\\' : '/' })
         ])
     }
 }
